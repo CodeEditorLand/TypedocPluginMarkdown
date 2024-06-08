e0fc19860195863e59ab2c833dc5003c646e653e
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-08 07:35:21 +0300

    squash!

--- packages/typedoc-plugin-markdown/src/partials/type.typeOperator.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOperatorType } from 'typedoc';
-import { MarkdownThemeRenderContext } from '../theme-context';
-
-export function typeOperatorType(
-  context: MarkdownThemeRenderContext,
-  model: TypeOperatorType,
-) {
-  return `${model.operator} ${context.partials.someType(model.target)}`;
-}
